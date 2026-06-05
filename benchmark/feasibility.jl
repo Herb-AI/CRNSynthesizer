847db90b82299316775d17ab34f4837a7ce458b7
Adam Szymaniak <a.p.szymaniak@student.tudelft.nl>
2026-06-05 16:47:46 +0200

    Uncomment benchmark runs

--- benchmark/feasibility.jl
+++ benchmark/feasibility.jl
@@ -266,7 +266,7 @@ end
 
 run_hardcoded_benchmarks()
 
-#run_automated_rbl_benchmark(; dataset = "complex", max_scan = 20, max_synthesis_runs = 10)
-#run_automated_rbl_benchmark(; dataset = "mbs", max_scan = 100, max_synthesis_runs = 10)
-#run_automated_rbl_benchmark(; dataset = "mnc", max_scan = 100, max_synthesis_runs = 10)
-#run_automated_rbl_benchmark(; dataset = "mos", max_scan = 100, max_synthesis_runs = 10)
+run_automated_rbl_benchmark(; dataset = "complex", max_scan = 300, max_synthesis_runs = 200)
+run_automated_rbl_benchmark(; dataset = "mbs", max_scan = 300, max_synthesis_runs = 200)
+run_automated_rbl_benchmark(; dataset = "mnc", max_scan = 300, max_synthesis_runs = 200)
+run_automated_rbl_benchmark(; dataset = "mos", max_scan = 300, max_synthesis_runs = 200)
